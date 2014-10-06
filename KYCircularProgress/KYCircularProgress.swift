78545909c976bdfe5b693c09204fc226b810049d
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2014-10-06 22:48:34 +0900

    Initial Commit.

---
+++ KYCircularProgress/KYCircularProgress.swift
@@ -0,0 +1,136 @@
+//
+//  KYCircularProgress.swift
+//  KYCircularProgress
+//
+//  Created by Y.K on 2014/10/02.
+//  Copyright (c) 2014年 Yokoyama Kengo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - KYCircularProgress
+class KYCircularProgress: UIView {
+    typealias progressChangedHandler = (progress: Double, circularView: KYCircularProgress) -> ()
+    let kShapeViewAnimation = "kShapeViewAnimation"
+    var progressChangedClosure: progressChangedHandler?
+    var progressView: KYCircularShapeView!
+    var progressDifference = 0
+    var gradientLayer: CAGradientLayer!
+    var progress: Double = 0.0 {
+        didSet(newValue) {
+            let clipProgress = max( min(newValue, 1.0), 0.0)
+            self.progressView.updateProgress(clipProgress)
+            
+            if let progressChanged = progressChangedClosure {
+                progressChanged(progress: clipProgress, circularView: self)
+            }
+        }
+    }
+    var startAngle: Double = 0.0 {
+        didSet {
+            self.progressView.startAngle = self.startAngle
+        }
+    }
+    var endAngle: Double = 0.0 {
+        didSet {
+            self.progressView.endAngle = self.endAngle
+        }
+    }
+    var lineWidth: Double = 8.0 {
+        didSet {
+            self.progressView.shapeLayer().lineWidth = CGFloat(self.lineWidth)
+        }
+    }
+    var path: UIBezierPath? {
+        didSet {
+            self.progressView.shapeLayer().path = self.path?.CGPath
+        }
+    }
+    var colors: [AnyObject]? {
+        didSet {
+            self.gradientLayer.colors = self.colors?
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    func setup() {
+        self.progressView = KYCircularShapeView(frame: self.bounds)
+        self.progressView.shapeLayer().fillColor = UIColor.clearColor().CGColor
+        self.progressView.shapeLayer().path = self.path?.CGPath
+        
+        gradientLayer = CAGradientLayer(layer: layer)
+        gradientLayer.frame = self.progressView.frame
+        gradientLayer.startPoint = CGPointMake(0, 0.5);
+        gradientLayer.endPoint = CGPointMake(1, 0.5);
+        gradientLayer.mask = self.progressView.shapeLayer();
+        gradientLayer.colors = self.colors ?? [colorHex(0x9ACDE7).CGColor!, colorHex(0xE7A5C9).CGColor!]
+        
+        self.layer.addSublayer(gradientLayer)
+        self.progressView.shapeLayer().strokeColor = self.tintColor.CGColor
+    }
+    
+    func progressChangedBlock(completion: progressChangedHandler) {
+        progressChangedClosure = completion
+    }
+    
+    func colorHex(rgb: Int) -> UIColor {
+        return UIColor(red: CGFloat((rgb & 0xFF0000) >> 16)/255.0,
+                       green: CGFloat((rgb & 0xFF00) >> 8)/255.0,
+                       blue: CGFloat(rgb & 0xFF)/255.0,
+                       alpha: 0.55)
+    }
+}
+
+// MARK: - KYCircularShapeView
+class KYCircularShapeView: UIView {
+    var startAngle = 0.0
+    var endAngle = 0.0
+    
+    override class func layerClass() -> AnyClass {
+        return CAShapeLayer.self
+    }
+    
+    func shapeLayer() -> CAShapeLayer {
+        return self.layer as CAShapeLayer
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.updateProgress(0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if self.startAngle == self.endAngle {
+            self.endAngle = self.startAngle + M_PI * 2
+        }
+        self.shapeLayer().path = self.shapeLayer().path ?? self.layoutPath().CGPath
+    }
+    
+    func layoutPath() -> UIBezierPath {
+        var halfWidth = CGFloat(self.frame.size.width / 2.0)
+        return UIBezierPath(arcCenter: CGPointMake(halfWidth, halfWidth), radius: halfWidth - self.shapeLayer().lineWidth, startAngle: CGFloat(self.startAngle), endAngle: CGFloat(self.endAngle), clockwise: true)
+    }
+    
+    func updateProgress(progress: Double) {
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        self.shapeLayer().strokeEnd = CGFloat(progress)
+        CATransaction.commit()
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,56 @@ KYCircularProgress
 ==================
 
 Flexible progress bar written in Swift.
+
+## Features
+- [x] Gradation Color
+- [x] UIBezierPath Progress Bar
+- [x] Progress Closure
+
+## Demo
+
+## Requirement
+* iOS7.0+
+* Xcode6.0.1 (Swift 1.0)
+
+## Usage
+#### Create KYCircularProgress
+```swift
+var circularProgress: KYCircularProgress! = KYCircularProgress(frame: self.bounds)
+```
+
+#### Gradation Progress Bar
+```swift
+// support Hex color to RGB color
+circularProgress.colors = [circularProgress.colorHex(0xA6E39D).CGColor!, circularProgress.colorHex(0xAEC1E3).CGColor!, circularProgress.colorHex(0xE1A5CB).CGColor!, circularProgress.colorHex(0xF3C0AB).CGColor!]
+```
+
+#### Progress Closure
+```swift
+circularProgress.progressChangedBlock({ (progress: Double, circular: KYCircularProgress) in
+	println("progress: \(progress)")
+})
+```
+
+#### UIBezierPath Progress Bar
+```swift
+// create "Star progress bar"
+let path = UIBezierPath()
+path.moveToPoint(CGPointMake(50.0, 2.0))
+path.addLineToPoint(CGPointMake(84.0, 86.0))
+path.addLineToPoint(CGPointMake(6.0, 33.0))
+path.addLineToPoint(CGPointMake(96.0, 33.0))
+path.addLineToPoint(CGPointMake(17.0, 86.0))
+path.closePath()
+circularProgress.path = path
+```
+## Installation
+1. Add `KYCircularProgress.swift` in your project.
+
+## Licence
+
+MIT
+
+## Author
+
+[kentya6](https://github.com/kentya6)
